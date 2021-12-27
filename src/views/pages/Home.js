372a326404460fd5728cd03be6ed7869fbffb335
Ismail Kılıç <ismail000728@gmail.com>
2021-12-27 18:41:58 +0300

    Table added on home page, delete item functionality added.

--- src/views/pages/Home.js
+++ src/views/pages/Home.js
@@ -1,15 +1,58 @@
+const getList = () => {
+    return JSON.parse(localStorage.getItem('my-list')) || [];
+}
+
+
+
 let Home = {
-    render : async () => {
+    render: async () => {
+        let list = getList()
+
         let view = `
             <section>
-                <h1> Home </h1>
+                <table id="playlist">
+                    <tr>
+                        <th>Id</th>
+                        <th>Name</th>
+                        <th>Weight</th>
+                        <th>Actions</th>
+                    </tr>
+                    ${list.map((a, index) =>
+                        `<tr>
+                            <td>${index + 1}.</td>
+                            <td>${a.name}</td>
+                            <td>Weight: ${a.weight}</td>
+                            <td>
+                                <button class="edit-btn" data-arg1=${index}>Edit</button>
+                                <button class="delete-btn" data-arg1=${index}>Delete</button>
+                            </td>
+                        </tr>`
+                        ).join('\n ')
+            }
+                    
+                </table>
+                
             </section>
         `
         return view
     }
     , after_render: async () => {
-    }
+        document.querySelectorAll(".edit-btn")
+            .forEach(btn => btn.addEventListener("click", e => {
+                console.log("edit, ", e.target.getAttribute('data-arg1'));
+            }));
 
+        document.querySelectorAll(".delete-btn")
+            .forEach(btn => btn.addEventListener("click", e => {
+                const index = e.target.getAttribute('data-arg1');
+                let values = JSON.parse(localStorage.getItem('my-list')) || []
+                
+                values.splice(index,1)
+
+                localStorage.setItem('my-list', JSON.stringify(values))
+                location.reload()
+        }));
+    }
 }
 
 export default Home;
