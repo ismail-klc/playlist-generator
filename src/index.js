3a394551ef1b29afe5bfe04207e2080f10644763
Ismail Kılıç <ismail000728@gmail.com>
2021-12-27 16:34:55 +0300

    Home page created, Routing implemented.

--- src/index.js
+++ src/index.js
@@ -1,1 +1,26 @@
+"use strict";
+
 import '../public/css/style.css'
+import Home from './views/pages/Home'
+import Utils from './services/Utils.js'
+
+const routes = {
+    '/': Home
+};
+
+const router = async () => {
+    const root = document.getElementById('root');
+    const content = document.getElementById('page_container');
+    const footer = document.getElementById('footer_container');
+
+    let request = Utils.parseRequestURL()
+
+    let parsedURL = (request.resource ? '/' + request.resource : '/') + (request.id ? '/:id' : '') + (request.verb ? '/' + request.verb : '')
+    
+    let page = Home
+    root.innerHTML = await page.render();
+    await page.after_render();
+}
+
+window.addEventListener('hashchange', router);
+window.addEventListener('load', router);
