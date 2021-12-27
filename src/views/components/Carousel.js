a82a763ad40eed24b7d86539dfafd23d4049795a
Ismail Kılıç <ismail000728@gmail.com>
2021-12-27 20:09:06 +0300

    Carousel extracted to a component.

---
+++ src/views/components/Carousel.js
@@ -0,0 +1,84 @@
+const getList = () => {
+    return JSON.parse(localStorage.getItem('my-list')) || [];
+}
+
+let Navbar = {
+    render: async () => {
+        let list = getList()
+
+        let view = `
+            <section>
+                
+
+                <div class="slideshow-container">
+
+                        ${list.map((a, index) =>
+            `
+                            <div class="mySlides fade">
+                                <div class="numbertext">${index + 1} / ${list.length}</div>
+                                <img src=${a.url} >
+                                <div class="text">${a.name}</div>
+                            </div>
+                            `
+        ).join('\n ')
+            }
+
+                        <a class="prev" >&#10094;</a>
+                        <a class="next" >&#10095;</a>
+                    </div>
+                    <br>
+
+                    <div style="text-align:center">
+                    ${list.map((a, index) =>
+                `
+                        <span class="dot" data-arg=${index + 1}></span> 
+                        `
+            ).join('\n ')
+            }
+                    
+                    </div>
+
+            </section>
+        `
+        return view
+    },
+    after_render: async () => { 
+        var slideIndex = 1;
+        showSlides(slideIndex);
+
+        document.querySelector(".prev").addEventListener('click', () => {
+            showSlides(slideIndex -= 1);
+        })
+
+        document.querySelector(".next").addEventListener('click', () => {
+            showSlides(slideIndex += 1);
+        })
+
+
+        document.querySelectorAll(".dot")
+            .forEach(btn => btn.addEventListener("click", e => {
+                const index = e.target.getAttribute('data-arg');
+                showSlides(slideIndex = index);
+            }));
+
+
+        function showSlides(n) {
+            var i;
+            var slides = document.getElementsByClassName("mySlides");
+            var dots = document.getElementsByClassName("dot");
+            if (n > slides.length) { slideIndex = 1 }
+            if (n < 1) { slideIndex = slides.length }
+            for (i = 0; i < slides.length; i++) {
+                slides[i].style.display = "none";
+            }
+            for (i = 0; i < dots.length; i++) {
+                dots[i].className = dots[i].className.replace(" active", "");
+            }
+            slides[slideIndex - 1].style.display = "block";
+            dots[slideIndex - 1].className += " active";
+        }
+    }
+
+}
+
+export default Navbar;
--- src/views/pages/Generate.js
+++ src/views/pages/Generate.js
@@ -1,83 +1,42 @@
-const getList = () => {
-    return JSON.parse(localStorage.getItem('my-list')) || [];
-}
+import Carousel from '../components/Carousel'
 
 let Generate = {
     render: async () => {
-        let list = getList()
-
         let view = `
-            <section>
-                <div style="margin-bottom: 50px; margin-top:20px"> </div>
-                <div class="slideshow-container">
-
-                        ${list.map((a, index) =>
-            `
-                            <div class="mySlides fade">
-                                <div class="numbertext">${index + 1} / ${list.length}</div>
-                                <img src=${a.url} >
-                                <div class="text">${a.name}</div>
-                            </div>
-                            `
-                        ).join('\n ')
-                            }
-
-                        <a class="prev" >&#10094;</a>
-                        <a class="next" >&#10095;</a>
-                    </div>
-                    <br>
-
-                    <div style="text-align:center">
-                    ${list.map((a, index) =>
-                        `
-                        <span class="dot" data-arg=${index+1}></span> 
-                        `
-                    ).join('\n ')
-                        }
-                    
-                    </div>
-
-            </section>
+        <div>
+            <div style="margin-bottom: 50px; margin-top:20px">
+                <button id="list-carousel">List</button>
+            </div>
+            <div id="simulation"></div>
+        </div>
         `
+        
         return view
     }
     , after_render: async () => {
-        var slideIndex = 1;
-        showSlides(slideIndex);
+        let btn = document.getElementById("list-carousel");
+        document.querySelector("#list-carousel").addEventListener('click', async (e) => {
+            console.log(btn.innerText);
+            if (btn.innerText === "List") {
+                btn.innerText = "Carousel";
 
-        document.querySelector(".prev").addEventListener('click', ()=>{
-            showSlides(slideIndex-=1);
+                document.querySelector('#simulation').innerHTML = `
+                    <div></div>
+                `
+            }
+            else {
+                btn.innerText = "List";
+
+                document.querySelector('#simulation').innerHTML = await Carousel.render();
+                await Carousel.after_render();
+            }
         })
 
-        document.querySelector(".next").addEventListener('click', ()=>{
-            showSlides(slideIndex+=1);
-        })
-
-
-        document.querySelectorAll(".dot")
-            .forEach(btn => btn.addEventListener("click", e => {
-                const index = e.target.getAttribute('data-arg');
-            showSlides(slideIndex = index);
-        }));
-
-
-        function showSlides(n) {
-            var i;
-            var slides = document.getElementsByClassName("mySlides");
-            var dots = document.getElementsByClassName("dot");
-            if (n > slides.length) { slideIndex = 1 }
-            if (n < 1) { slideIndex = slides.length }
-            for (i = 0; i < slides.length; i++) {
-                slides[i].style.display = "none";
-            }
-            for (i = 0; i < dots.length; i++) {
-                dots[i].className = dots[i].className.replace(" active", "");
-            }
-            slides[slideIndex - 1].style.display = "block";
-            dots[slideIndex - 1].className += " active";
+        if (btn.innerText === "List") {
+            document.querySelector('#simulation').innerHTML = await Carousel.render();
+            await Carousel.after_render();
         }
     }
-
 }
 
 export default Generate;
