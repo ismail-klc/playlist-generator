c28ff49b70e2d5f21da76da5de781e0cd0cb2eee
Ismail Kılıç <ismail000728@gmail.com>
2021-12-27 15:48:26 +0300

    Project created, Webpack and Babel configured.

---
+++ webpack.config.js
@@ -0,0 +1,42 @@
+const path = require('path')
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+    entry: './src/index.js',
+    output: {
+        path: path.resolve(__dirname, 'dist'),
+        filename: 'js/bundle.js'
+    },
+    module: {
+        rules: [
+            {
+                test: /\.m?js$/,
+                exclude: /node_modules/,
+                use: {
+                    loader: "babel-loader",
+                    options: {
+                        presets: ['@babel/preset-env']
+                    }
+                }
+            },
+            {
+                test: /\.css$/i,
+                use: ["style-loader", "css-loader"],
+            },
+        ]
+    },
+    devServer: {
+        static: {
+            directory: path.join(__dirname, 'dist'),
+        },
+        compress: true,
+        port: 9000,
+    },
+    plugins: [new HtmlWebpackPlugin({
+        filename: "index.html",
+        template: "./public/index.html",
+        minify: true,
+        favicon: "./public/favicon.ico"
+    })],
+
+}
